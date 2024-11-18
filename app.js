64cebab0a5e81490e589f0057ae0d9ffc0ea78d3
MattiaEsposito98 <esposito.matty@hotmail.it>
2024-11-18 15:13:17 +0100

    add express

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log ("express")
+console.log ("Blog")
+const express = require ('express')
+const app = express ()
+const port = 3000 
+
+
+app.get ('/', (req, res) => {
+  res.send ('Server del mio blog')
+})
+
+
+app.listen (port, () => {
+  console.log (`Porta in ascolto ${port}`)
+})
+
